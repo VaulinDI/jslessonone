245f0fceebc8754b5f49d5f740e846664796284f
dmitry <vdi85@mail.ru>
2023-01-04 15:02:19 +0700

    script file added

---
+++ script.js
@@ -0,0 +1,9 @@
+let celsius = 30;
+let fahrenheit = (9 / 5) * celsius + 32;
+console.log(`Температура в градусах Цельсия: ${celsius}`);
+console.log(`Температура по Фаренгейту: ${fahrenheit}`);
+console.log("\n");
+
+let name = 'Дмитрий';
+let admin = name;
+console.log(`admin = ${admin}`);
